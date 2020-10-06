2ca95a2e293479ca1cb190134faa0f676b078da5
Youn Hee Pernling Frodin <yonipernling@gmail.com>
2020-10-06 15:34:44 -0400

    prompt and basic document info

---
+++ js/p3-prompt-variables.js
@@ -0,0 +1,16 @@
+
+var name;
+name = prompt("Enter your name?"); // get name from user
+
+document.write("This is your name in small size: " + name);               // write name that user inputted
+
+document.write("<h1>" + "This is your name in large size: " + name + "</h1>"); // write name that user inputted, with h1 size
+
+
+
+var date = Date();  // declare date variable, Date() is collected from computer 
+
+document.write("This is the time of date right now: " + date+"<br>");    // show todays date with date variable 
+
+document.write("This is the title of the page: " + document.title+"<br>"); // show the title of page 
+document.write("This is the URL of the page: " + document.URL+"<br>"); // show the URL of page 
